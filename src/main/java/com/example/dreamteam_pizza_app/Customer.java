d2600f3a52268730fb4df0facb48985a96bae4fe
lukeklusty <42556016+lukeklusty@users.noreply.github.com>
2022-11-12 01:47:56 -0700

    Added customer page base functionality.

--- src/main/java/com/example/dreamteam_pizza_app/Customer.java
+++ src/main/java/com/example/dreamteam_pizza_app/Customer.java
@@ -1,25 +1,39 @@
 package com.example.dreamteam_pizza_app;
 
+import java.util.ArrayList;
+
 public class Customer {
 
-    int asuID;
-    String emailAddress;
-    PizzaOrder Pizza;
+    private int asuID;
+    private ArrayList<PizzaOrder> pizzaOrderList = new ArrayList<PizzaOrder>();
+
+    public Customer(int asuIDNumber)
+    {
+        asuID = asuIDNumber;
+    }
+
+    public ArrayList<PizzaOrder> getPizzaOrderList()
+    {
+        return pizzaOrderList;
+    }
+
+    public void addPizzaOrder(PizzaOrder pizzaOrder)
+    {
+        pizzaOrderList.add(pizzaOrder);
+    }
+
+    public int getAsuID() {
+        return asuID;
+    }
 
     boolean orderFinished(){
         return true;
     }
 
-    boolean validateStudent_Email(){
-        if (emailAddress.contains("@") && emailAddress.contains(".edu")) {
-            return true;
-        }else {
-            return false;
-        }
-    }
+
 
     boolean validateStudentASU_ID(){
-        if (asuID > 100000000 && asuID < 999999999) {
+        if (asuID > 100000000 && asuID < 2147483646) {
             return true;
         }else {
             return false;
--- src/main/java/com/example/dreamteam_pizza_app/PizzaOrder.java
+++ src/main/java/com/example/dreamteam_pizza_app/PizzaOrder.java
@@ -1,12 +1,29 @@
 package com.example.dreamteam_pizza_app;
 
+import java.util.ArrayList;
+
 public class PizzaOrder {
     String pizzaType;
-    String pizzaToppings;
+    ArrayList<String> pizzaToppings;
     String pizzaPickupTime;
+    String pizzaPickupTimeAmOrPm;
+    String orderStatus = "";
 
+    public PizzaOrder(String pizzaType, ArrayList<String> toppings, String pickupTime, String amOrPm)
+    {
+        this.pizzaType = pizzaType;
+        this.pizzaToppings = toppings;
+        this.pizzaPickupTime = pickupTime;
+        this.pizzaPickupTimeAmOrPm = amOrPm;
+        this.orderStatus = "Ready to Cook";
+    }
     boolean changeOrderStatus(){
         return true;
+    }
+
+    public String getOrderStatus()
+    {
+        return orderStatus;
     }
 
     String orderHistory(){
